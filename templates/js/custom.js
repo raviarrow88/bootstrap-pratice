ab38fdaaf1e4665774ca0bbae60b896baf7b9050
Ravi Teja <raviarrow88@gmail.com>
2017-03-02 15:15:23 +0530

    smooth scrolling effect added

---
+++ templates/js/custom.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+
+$('.slideSection').click(function(e)
+{
+var linkHref = $(this).attr('href');
+$('html,body').animate({
+
+scrollTop: $(linkHref).offset().top
+},1000);
+
+e.preventDefault();
+});
+
+});
